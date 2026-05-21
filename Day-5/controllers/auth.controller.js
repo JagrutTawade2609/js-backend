736e36312a33a16a487d1e8c0f71046374d50989
JagrutTawade2609 <jagruttawade38855@gmail.com>
2026-05-21 23:56:22 +0530

    get products lec 14

--- Day-5/controllers/auth.controller.js
+++ Day-5/controllers/auth.controller.js
@@ -20,18 +20,19 @@ export const Register = async (req, res) => {
         await user.save(); // Save the user to the database
         res.status(201).json({ 
             message: "user registered successfully", 
+            success: true,
             user: { name: name, email: email, password: hashedPassword, role: role } // Return the user details in the response (excluding sensitive information like password)
         
         })
     }catch(error){
-        return res.status(500).json({message: "Error registering user", error: error.message})
+        return res.status(500).json({message: "Error registering user", error: error.message, success: false})
     }
 }
 export const Login = async (req, res) => {
     try {
         const { email, password } = req.body; // Extract email and password from the request body
         if (!email || !password) {
-            return res.status(400).json({ message: "Email and password are required" }); // If email or password is missing, return a 400 Bad Request response with an error message
+            return res.status(400).json({ message: "Email and password are required", success: false }); // If email or password is missing, return a 400 Bad Request response with an error message
         }
         const user = await UserModel.findOne({ email: email} ); // Find the user in the database by email
         if (!user) {
@@ -53,9 +54,9 @@ export const Login = async (req, res) => {
             email: user.email,
             role: user.role
         }; // Prepare the user data to be sent in the response (excluding sensitive information like password)  
-        return res.status(200).json({ token, message: "Login successful", user: userData }); // If login is successful, return a 200 OK response with a success message and user details (excluding sensitive information like password)
+        return res.status(200).json({ success: true, token, message: "Login successful", user: userData }); // If login is successful, return a 200 OK response with a success message and user details (excluding sensitive information like password)
     } catch (error) {
-        return res.status(500).json({ message: "Error logging in", error: error.message }); // If an error occurs during the login process, return a 500 Internal Server Error response with an error message
+        return res.status(500).json({ message: "Error logging in", error: error.message, success: false }); // If an error occurs during the login process, return a 500 Internal Server Error response with an error message
     }
 };
 
@@ -63,17 +64,17 @@ export const GetCurrentUser = async (req, res) => {
     try {
         const token = req.cookies.token; // Retrieve the token from the request cookies
         if (!token) {
-            return res.status(401).json({ message: "Unauthorized" }); // If no token is found, return a 401 Unauthorized response with an error message
+            return res.status(401).json({ message: "Unauthorized", success: false }); // If no token is found, return a 401 Unauthorized response with an error message
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await UserModel.findById(decoded.userId).select("-password"); // Find the user in the database by ID and exclude the password field
         if (!user) {
-            return res.status(404).json({ message: "User not found" }); // If the user is not found, return a 404 Not Found response with an error message
+            return res.status(404).json({ message: "User not found", success: false }); // If the user is not found, return a 404 Not Found response with an error message
         }
-        return res.status(200).json({ user }); // If the user is found, return a 200 OK response with the user details (excluding sensitive information like password)
+        return res.status(200).json({ success: true, user }); // If the user is found, return a 200 OK response with the user details (excluding sensitive information like password)
     }
     catch (error) {
-        return res.status(500).json({ message: "Error retrieving user", error: error.message }); // If an error occurs during the process, return a 500 Internal Server Error response with an error message
+        return res.status(500).json({ message: "Error retrieving user", error: error.message, success: false }); // If an error occurs during the process, return a 500 Internal Server Error response with an error message
     }
 };
 
@@ -86,8 +87,8 @@ export const UpdateUserPassword = async (req, res) => {
                 await UserModel.findByIdAndUpdate(users[i]._id, { password: hashedPassword }); // Update the user's password in the database with the hashed version
             }
         }
-        return res.status(200).json({ message: "Passwords updated successfully" }); // Return a success message in the response    
+        return res.status(200).json({ message: "Passwords updated successfully", success: true }); // Return a success message in the response    
     } catch (error) {
-        return res.status(500).json({ message: "Error updating password", error: error.message }); // If an error occurs, return a 500 Internal Server Error response with an error message
+        return res.status(500).json({ message: "Error updating password", error: error.message, success: false }); // If an error occurs, return a 500 Internal Server Error response with an error message
     }
 };
